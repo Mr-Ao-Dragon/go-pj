d7dab32bad5d272b51f4ba62791349def3ca77f5
MrAoDragon <x123456789fy@outlook.com>
2022-03-29 14:01:50 +0800

    lol

--- src/main.go
+++ src/main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
+//
 //import "github.com/beego/beego/v2/server/web"
 func main() {
 	sum := 0
@@ -11,6 +8,5 @@ loop:
 	for i := 0; i <= 2; i++ {
 		sum += i
 	}
-	fmt.Println(sum)
 	goto loop
 }
